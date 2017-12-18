9f9d87ba5219a232bcff7e2378788a58b2c1f3fb
baku171 <pbak@esri.pl>
2017-12-18 10:54:40 +0100

    App component

--- client/App.js
+++ client/App.js
@@ -16,6 +16,31 @@ class App extends Component {
         this.state = {users: [], messages: [], text: '', name: ''};
     }
 
+    componentDidMount() {
+        socket.on('message', message => this.messageReceive(message));
+        socket.on('update', ({users}) => this.chatUpdate(users));
+    }
+
+    messageReceive(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({messages});
+    }
+
+    chatUpdate(users) {
+        this.setState({users});
+    }
+
+    handleMessageSubmit(message) {                                  // nie można do tego wykorzystać już istniejącej metody messageReceive??
+        const messages = [message, ...this.state.messages];
+        this.setState({messages});
+        socket.emit('message', message);
+    }
+
+    handleUserSubmit(name) {
+        this.setState({name});
+        socket.emit('join', name);
+    }
+
     render() {
         return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
     }
