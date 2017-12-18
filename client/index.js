775afaff63d5ec1c6902ea26772cd03c24e14227
baku171 <pbak@esri.pl>
2017-12-18 10:14:34 +0100

    Initial commit

---
+++ client/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {AppContainer} from 'react-hot-loader';
+
+import App from './App';
+import { rename } from 'fs';
+
+const render = (Component) => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        document.getElementById('root')
+    );
+};
+
+render(App);
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        const NewApp = require('./App').default;
+        render(NewApp)
+    });
+}
